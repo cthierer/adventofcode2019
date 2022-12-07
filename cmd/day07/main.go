971512dd92d067b373d89833ca206771869964fa
Chris Thierer <cthierer1@gmail.com>
2022-12-07 08:53:37 -0500

    day 7, part 2

--- cmd/day07/main.go
+++ cmd/day07/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/cthierer/advent-of-code/device/filesystem"
 	"github.com/cthierer/advent-of-code/device/shell"
 )
 
@@ -28,4 +29,19 @@ func main() {
 	}
 
 	fmt.Printf("Total size of directories with at most 100000: %v\n", sum)
+
+	freeSpace := 70000000 - s.FS.Root().TotalSize()
+	needToFree := 30000000 - freeSpace
+
+	var smallestDirInThreshold *filesystem.Dir
+	for _, dir := range s.FS.Dirs() {
+		size := dir.TotalSize()
+		if size >= needToFree {
+			if smallestDirInThreshold == nil || size < smallestDirInThreshold.TotalSize() {
+				smallestDirInThreshold = dir
+			}
+		}
+	}
+
+	fmt.Printf("Smallest directory that can free up enough space (need %v): %s (%v)\n", needToFree, smallestDirInThreshold.Name, smallestDirInThreshold.TotalSize())
 }
