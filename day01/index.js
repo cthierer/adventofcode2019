974ae70d47044368319c8070b1e9786839587c30
Chris Thierer <cthierer@users.noreply.github.com>
2024-12-04 12:45:40 +0000

    day 1, part 1

---
+++ day01/index.js
@@ -0,0 +1,72 @@
+
+const appendToSorted = (list, value) => {
+    if (list.length < 1) {
+        return [value]
+    }
+
+    const idx = list.findLastIndex((element) => element < value)
+
+    if (idx < 0) {
+        return [value, ...list]
+    }
+
+    return [
+        ...list.slice(0, idx + 1),
+        value,
+        ...list.slice(idx + 1),
+    ]
+}
+
+const parseInt = (valueStr) => {
+    const valueInt = Number.parseInt(valueStr, 10)
+    if (Number.isNaN(valueInt)) {
+        return new Error(`invalid number: "${valueStr}"`)
+    }
+
+    return valueInt
+}
+
+const computeDistances = (list1, list2) => list1.map((value1, idx) => {
+    if (idx > list2.length) {
+        return value1
+    }
+
+    const value2 = list2[idx]
+    return Math.abs(value1 - value2)
+})
+
+const run = (input) => {
+    let list1 = []
+    let list2 = []
+
+    input.on('readable', () => {
+        let chunk
+        while ((chunk = input.read()) && chunk !== null) {
+            const lines = chunk.toString('utf8').trim().split('\n')
+            lines.forEach((element) => {
+                const line = element.trim() 
+                if (line.length < 1) {
+                    return
+                }
+
+                const [list1Str, list2Str] = line.split(/\s+/)
+                list1 = appendToSorted(list1, parseInt(list1Str))
+                list2 = appendToSorted(list2, parseInt(list2Str))    
+            })
+        }
+    })
+
+    input.on('error', (err) => {
+        console.error(`An error occurred: ${err.message}`)
+    })
+
+    input.on('end', () => {
+        const deltas = computeDistances(list1, list2)
+        const sum = deltas.reduce((last, curr) => last += curr, 0)
+
+        console.log('Total distance:\t', sum)
+        console.log('Done!')
+    })
+}
+
+run(process.stdin)
