3138b7928470d53f1eda7dabf4a5b8b640d791e3
Chris Thierer <cthierer@users.noreply.github.com>
2024-12-06 02:31:11 +0000

    day 3, part 2

--- day03/index.js
+++ day03/index.js
@@ -3,6 +3,39 @@ import run from '../util/run.mjs'
 
 const runner = run(process.stdin)
 
+const findNextOccurrence = (string, pattern, afterIdx) => {
+    let nextOccurrence = afterIdx - 1
+
+    while (nextOccurrence < afterIdx) {
+        const match = pattern.exec(string)
+        if (match === null) {
+            return string.length
+        }
+
+        nextOccurrence = match.index + 1
+    }
+
+    return nextOccurrence
+}
+
+const parseBlock = function* parseBlock(string, initialState) {
+    const enablePattern = /do\(\)/g
+    const disablePattern = /don't\(\)/g
+    const { enabled } = initialState
+    let startIdx = enabled ? 0 : findNextOccurrence(string, enablePattern, 0)
+    let stopIdx
+
+    while (startIdx < string.length) {
+        stopIdx = findNextOccurrence(string, disablePattern, startIdx)
+        yield string.substring(startIdx, stopIdx)
+        
+        startIdx = findNextOccurrence(string, enablePattern, stopIdx)
+    }
+
+    return { ...initialState, enabled: stopIdx >= startIdx }
+}
+
+
 const parseMul = function* parseMul(string) {
     const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g
     let match
@@ -15,12 +48,30 @@ const parseMul = function* parseMul(string) {
 
 const execMul = ([x, y]) => x * y
 
-let total = 0
+const sum = (last, curr) => last += curr
 
+let totalPt1 = 0
 runner.onLine((line) => {
-    total += Array.from(parseMul(line)).map(execMul).reduce((last, curr) => last += curr, 0)
+    totalPt1 = Array.from(parseMul(line)).map(execMul).reduce(sum, totalPt1)
+})
+
+let totalPt2 = 0
+let state = { enabled: true }
+runner.onLine((line) => {
+    const blocks = parseBlock(line, state)
+    let result
+
+    while ((result = blocks.next()) && !result.done) {
+        const block = result.value
+        for(const args of parseMul(block)) {
+            totalPt2 += execMul(args)
+        }
+    }
+
+    state = result.value
 })
 
 runner.onEnd(() => {
-    console.log(`Total:\t${total}`)
+    console.log(`Total (part 1):\t${totalPt1}`)
+    console.log(`Total (part 2):\t${totalPt2}`)
 })
